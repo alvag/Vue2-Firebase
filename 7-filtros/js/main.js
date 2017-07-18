6ea37cc0488ad25f6bd5de981597d00bfec33c26
Max Alva <alva85@gmail.com>
2017-07-18 15:47:32 -0500

    Filtros

---
+++ 7-filtros/js/main.js
@@ -0,0 +1,35 @@
+Vue.filter('alRevez', (valor) => valor.split('').reverse().join(''));
+
+new Vue({
+    el: 'main',
+    data: {
+        busqueda: '',
+        minimo: 5,
+        juegos: [
+            {
+                titulo: 'Battlefield',
+                genero: 'FPS',
+                puntuacion: 9
+            },
+            {
+                titulo: 'Civilization VI',
+                genero: 'Estrategia',
+                puntuacion: 10
+            },
+            {
+                titulo: 'Resident Evil 7',
+                genero: 'Survival Horror',
+                puntuacion: 7
+            },
+        ]
+    },
+    
+    computed: {
+        mejoresJuegos(){
+            return this.juegos.filter((juego) => juego.puntuacion >= this.minimo);
+        },
+        buscarJuego(){
+            return this.juegos.filter((juego) => juego.titulo.includes(this.busqueda));
+        }
+    }
+});
