3e876b6d74249616eae2ad18084535a8bcaf8b88
Max Alva <alva85@gmail.com>
2017-07-18 23:03:50 -0500

    Data binding en atributos y clases

---
+++ 9-data-binding-atributos-clases/js/main.js
@@ -0,0 +1,22 @@
+const vm1 = new Vue({
+    el: 'main',
+    data: {
+        tareas: [
+            { titulo: 'Hacer la compra', completado: false},
+            { titulo: 'Aprender Vue', completado: false},
+            { titulo: 'Aprender Firebase', completado: false},
+            { titulo: 'Dominar ES6', completado: false},
+            { titulo: 'Salir a correr', completado: false},
+        ]
+    },
+    methods: {
+        completarTarea(tarea){
+            tarea.completado = !tarea.completado;
+        }
+    },
+    computed:{
+        tareasCompletadas(){
+            return this.tareas.filter((tarea) => tarea.completado);
+        }
+    }
+});
