54b11db822035112b7c19fece5ecff1bfc5581a9
Max Alva <alva85@gmail.com>
2017-07-18 23:53:41 -0500

    Ajax con vue-resource

---
+++ 11-ajax-vue-resource/js/main.js
@@ -0,0 +1,18 @@
+new Vue({
+    el: 'main',
+    mounted(){
+        this.cargarPersonas();
+    },
+    data: {
+        personas: []
+    },
+    methods: {
+        cargarPersonas(){
+            this.$http.get('https://randomuser.me/api/?results=50')
+                .then((respuesta) => {
+                    this.personas = respuesta.body.results;
+                })
+        }
+    }
+    
+});
