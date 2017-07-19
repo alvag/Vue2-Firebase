98849a926757f135a1c7094d148d8895b2da3d37
Max Alva <alva85@gmail.com>
2017-07-18 22:39:06 -0500

    La instancia de Vue

---
+++ 8-instancia-vue/js/main.js
@@ -0,0 +1,26 @@
+const vm1 = new Vue({
+    el: 'main',
+    data: {
+        mensaje: 'Instancia Vue 1'
+    },
+
+    methods: {
+        alReves() {
+            this.mensaje = this.mensaje.split('').reverse().join('');
+            vm2.mensaje = 'Hola desde instancia 1';
+        }
+    },
+
+    computed: {
+        mensajeMayusculas(){
+            return this.mensaje.toUpperCase();
+        }
+    }
+});
+
+const vm2 = new Vue({
+   el: '#app' ,
+   data: {
+       mensaje: 'Instancia Vue 2'
+   }
+});
